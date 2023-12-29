84f9bdb7ae3b1f98e2213ece7e9c5f6c25c91059
Edu-Lefranc <105300446+EdLefranc@users.noreply.github.com>
2023-12-29 19:33:26 -0300

    Update Compras.java

--- Gestion_Ventas/src/Entidades/Compras.java
+++ Gestion_Ventas/src/Entidades/Compras.java
@@ -5,10 +5,64 @@
  */
 package Entidades;
 
+import java.util.List;
+
 /**
  *
  * @author Ed Le Franc
  */
 public class Compras {
+    private int codCompra;
+    private RRHH provedor;
+    private List<Producto> producto;
+    private double monto;
+
+    public Compras() {
+    }
+
+    public Compras(int codCompra, RRHH provedor, List<Producto> producto, double monto) {
+        this.codCompra = codCompra;
+        this.provedor = provedor;
+        this.producto = producto;
+        this.monto = monto;
+    }
+
+    public int getCodCompra() {
+        return codCompra;
+    }
+
+    public void setCodCompra(int codCompra) {
+        this.codCompra = codCompra;
+    }
+
+    public RRHH getProvedor() {
+        return provedor;
+    }
+
+    public void setProvedor(RRHH provedor) {
+        this.provedor = provedor;
+    }
+
+    public List<Producto> getProducto() {
+        return producto;
+    }
+
+    public void setProducto(List<Producto> producto) {
+        this.producto = producto;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    @Override
+    public String toString() {
+        return "Compras{" + "codCompra=" + codCompra + ", provedor=" + provedor + ", producto=" + producto + ", monto=" + monto + '}';
+    }
+    
     
 }
